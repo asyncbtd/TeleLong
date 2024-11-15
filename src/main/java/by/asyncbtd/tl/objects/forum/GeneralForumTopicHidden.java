a49387f319a17064ebc5ec2c5682098a115b0f9d
asyncbtd <asyncbtd@gmail.com>
2024-11-16 00:48:52 +0300

    add object GeneralForumTopicHidden

---
+++ src/main/java/by/asyncbtd/tl/objects/forum/GeneralForumTopicHidden.java
@@ -0,0 +1,10 @@
+
+
+package by.asyncbtd.tl.objects.forum;
+
+/**
+ * This object represents a service message about General forum topic hidden in the chat. Currently holds no information.
+ * https://core.telegram.org/bots/api#generalforumtopichidden
+ */
+public class GeneralForumTopicHidden {
+}

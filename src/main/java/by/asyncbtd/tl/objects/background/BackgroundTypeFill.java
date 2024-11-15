ad556022fbbc52307900128d01693cb31fe6796b
asyncbtd <asyncbtd@gmail.com>
2024-11-16 00:17:38 +0300

    add object BackgroundTypeFill

---
+++ src/main/java/by/asyncbtd/tl/objects/background/BackgroundTypeFill.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2024 asyncbtd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package by.asyncbtd.tl.objects.background;
+
+public class BackgroundTypeFill {
+
+    private String type;
+    private BackgroundFill fill;
+    private Integer darkThemeDimming;
+
+    public String getType() {
+        return type;
+    }
+
+    public BackgroundFill getFill() {
+        return fill;
+    }
+
+    public Integer getDarkThemeDimming() {
+        return darkThemeDimming;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setFill(BackgroundFill fill) {
+        this.fill = fill;
+    }
+
+    public void setDarkThemeDimming(Integer darkThemeDimming) {
+        this.darkThemeDimming = darkThemeDimming;
+    }
+
+    public String toString() {
+        return "BackgroundTypeFill(type=" + this.getType() +
+                ", fill=" + this.getFill() +
+                ", darkThemeDimming=" + this.getDarkThemeDimming() + ")";
+    }
+
+    public BackgroundTypeFill() {
+    }
+
+    public BackgroundTypeFill(String type, BackgroundFill fill, Integer darkThemeDimming) {
+        this.type = type;
+        this.fill = fill;
+        this.darkThemeDimming = darkThemeDimming;
+    }
+}

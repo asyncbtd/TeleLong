e9380089bb07be619fcadafda46fbbfb2197bfa7
asyncbtd <asyncbtd@gmail.com>
2024-11-16 04:24:20 +0300

    add object ChatMemberLeft

---
+++ src/main/java/by/asyncbtd/tl/objects/chat/ChatMemberLeft.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 asyncbtd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package by.asyncbtd.tl.objects.chat;
+
+import by.asyncbtd.tl.objects.User;
+
+public class ChatMemberLeft implements ChatMember {
+
+    private String status;
+    private User user;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String toString() {
+        return "ChatMemberLeft(status=" + this.getStatus() +
+                ", user=" + this.getUser() + ")";
+    }
+
+    public ChatMemberLeft() {
+    }
+
+    public ChatMemberLeft(String status, User user) {
+        this.status = status;
+        this.user = user;
+    }
+}
